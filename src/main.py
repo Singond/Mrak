c9de20dd9761eda03c7c83321e81b0ff3e9c5950
Jan Singon Slany <singond@seznam.cz>
2020-11-26 00:22:17 +0100

    Enable specifying config file from command line

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
-from core import Mrak
+import argparse
 from log import logger
+from core import Mrak
 import systray
 
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("--config-file")
+args = parser.parse_args()
+
 logger.info("Starting Mrak")
-m = Mrak("../test/testconfig.yaml")
+logger.debug("Parsed arguments: %s", args)
+if "config_file" in args:
+    m = Mrak(args.config_file)
+else:
+    m = Mrak()
 systray.run(m)
 logger.info("Quitting Mrak")
